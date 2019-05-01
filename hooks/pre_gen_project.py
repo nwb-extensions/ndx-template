bfa534e97d9a31542701ccca216fd153f45a8fcf
Ryan Ly <rly@lbl.gov>
2019-05-01 16:41:26 -0700

    Add basic validation of cookiecutter inputs

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,29 @@
+import re
+import sys
+from email.utils import parseaddr
+
+PREF_NAMESPACE_REGEX = r'^[\-a-z]+$'
+REQ_NAMESPACE_REGEX = r'^[\-_a-zA-Z0-9]+$'
+GITHUB_USERNAME_REGEX = r'/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i'
+
+namespace = '{{ cookiecutter.namespace }}'
+email = '{{ cookiecutter.email }}'
+github_name = '{{ cookiecutter.github_name }}'
+
+if namespace[:4] != 'ndx-':
+    print('WARNING: It is recommended to prefix your NDX namespace with "ndx-".')
+if not re.match(PREF_NAMESPACE_REGEX, namespace):
+    print('WARNING: It is recommended to use only lower-case ASCII letters and '
+          'hyphens in your namespace.')
+if not re.match(REQ_NAMESPACE_REGEX, namespace):
+    # these are the rules for naming GitHub repositories, and we want the name
+    # of the GitHub repository to match the name of the extension
+    print('ERROR: Namespace can only have lower-case and upper-case ASCII '
+          'letters, hyphens (-), and underscores (-).')
+    sys.exit(1)
+if '@' not in parseaddr(email)[1]:
+    print(f'WARNING: The email address you entered "{email}" does not appear to '
+          'be a valid email address. Are you sure you entered it correctly?')
+if not re.match(GITHUB_USERNAME_REGEX, github_name):
+    print(f'WARNING: The GitHub username you entered "{email}" does not appear '
+          'to be a valid GitHub username. Are you sure you entered it correctly?')
