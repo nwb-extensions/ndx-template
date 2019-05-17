7e709ab68c69624063f2d28163a030913a4b093a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-05-17 04:10:47 -0400

    Re-factor hooks as modules with re-usable functions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,28 @@
 import os
 import shutil
+import textwrap
 
-dev_language = '{{ cookiecutter.dev_language }}'
-if dev_language != 'Python':
-    shutil.rmtree(os.path.join(os.getcwd(), 'src', 'pynwb'))
-if dev_language != 'MATLAB':
-    shutil.rmtree(os.path.join(os.getcwd(), 'src', 'matnwb'))
 
-print('\nSuccess! Directory {{ cookiecutter.namespace }} was created with a skeleton for your new NWB:N extension.\n'
-      'Please see {{ cookiecutter.namespace }}/NEXTSTEPS.md for creating and publishing your extension.')
+def _select_dev_language():
+    dev_language = '{{ cookiecutter.dev_language }}'
+    if dev_language != 'Python':
+        shutil.rmtree(os.path.join(os.getcwd(), 'src', 'pynwb'))
+    if dev_language != 'MATLAB':
+        shutil.rmtree(os.path.join(os.getcwd(), 'src', 'matnwb'))
+
+
+def main():
+    """
+    Runs the post gen project hook main entry point.
+    """
+    _select_dev_language()
+    print(textwrap.dedent(
+        """
+        Success! Directory {{ cookiecutter.namespace }} was created with a skeleton for your new NWB:N extension.
+        Please see {{ cookiecutter.namespace }}/NEXTSTEPS.md for creating and publishing your extension.
+        """
+    ))
+
+
+if __name__ == "__main__":
+    main()
