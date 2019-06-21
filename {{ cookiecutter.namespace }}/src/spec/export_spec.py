f989ee2036af71f821751e17c38dbf7fba159f85
Ryan Ly <rly@lbl.gov>
2019-06-20 18:05:41 -0700

    Fixes #10

--- {{ cookiecutter.namespace }}/src/spec/export_spec.py
+++ {{ cookiecutter.namespace }}/src/spec/export_spec.py
@@ -19,16 +19,16 @@ def export_spec(ns_builder, new_data_types):
     project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     output_dir = os.path.join(project_dir, 'spec')
 
-    ns_path = ns_builder._NamespaceBuilder__ns_args['name'] + '.namespace.yaml'
-    ext_path = ns_builder._NamespaceBuilder__ns_args['name'] + '.extensions.yaml'
-    
+    ns_path = ns_builder.name + '.namespace.yaml'
+    ext_path = ns_builder.name + '.extensions.yaml'
+
     if len(new_data_types) > 1:
         pluralize = 's'
     else:
         pluralize = ''
 
     print('Creating file {output_dir}/{ext_path} with {new_data_types_count} data type{pluralize}'.format(
-        pluralize=pluralize, output_dir=output_dir, ext_path=ext_path, 
+        pluralize=pluralize, output_dir=output_dir, ext_path=ext_path,
         new_data_types_count=len(new_data_types)))
 
     for neurodata_type in new_data_types:
