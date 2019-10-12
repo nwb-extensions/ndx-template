e0f1e416e2629dfad048f39eca2927494fe434c6
Ryan Ly <rly@lbl.gov>
2019-10-11 23:51:25 -0700

    Fix variable, change indenting to work with template

--- {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
+++ {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
@@ -1,13 +1,21 @@
 import os
 from pynwb import load_namespaces
+
 # Set path of the namespace.yaml file to the expected install location
-{{ cookiecutter.py_pkg_name }}_specpath = os.path.join(os.path.dirname(__file__), 
-                                                       'spec', 
-                                                       '{{ cookiecutter.namespace }}.namespace.yaml')
-# If the extensions has not been installed yet but we running directly from the git repo
-if not os.path.exists(ndx_icephys_meta_specpath):
-    {{ cookiecutter.py_pkg_name }}_specpath  = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                                            '../../../spec/',
-                                                                            '{{ cookiecutter.namespace }}.namespace.yaml'))
+{{ cookiecutter.py_pkg_name }}_specpath = os.path.join(
+    os.path.dirname(__file__), 
+    'spec', 
+    '{{ cookiecutter.namespace }}.namespace.yaml'
+)
+
+# If the extension has not been installed yet but we are running directly from the git repo
+if not os.path.exists({{ cookiecutter.py_pkg_name }}_specpath):
+    {{ cookiecutter.py_pkg_name }}_specpath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        '..', '..', '..', 
+        'spec',
+        '{{ cookiecutter.namespace }}.namespace.yaml'
+    ))
+
 # Load the namespace 
 load_namespaces({{ cookiecutter.py_pkg_name }}_specpath)
