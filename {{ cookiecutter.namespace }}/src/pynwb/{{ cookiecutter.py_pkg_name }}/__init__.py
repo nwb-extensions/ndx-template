55dbcd66f45d3e752fa7932a9cb3d26d5eb71539
Oliver Ruebel <oruebel@users.noreply.github.com>
2019-10-11 22:26:23 -0700

    Allow import directly from git rep
    Update the default setup.py to support usage of extensions directly from the GitHub repo without installing the extension yet. This is useful during development.

--- {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
+++ {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
@@ -1,4 +1,13 @@
 import os
 from pynwb import load_namespaces
-{{ cookiecutter.py_pkg_name }}_specpath = os.path.join(os.path.dirname(__file__), 'spec', '{{ cookiecutter.namespace }}.namespace.yaml')
+# Set path of the namespace.yaml file to the expected install location
+{{ cookiecutter.py_pkg_name }}_specpath = os.path.join(os.path.dirname(__file__), 
+                                                       'spec', 
+                                                       '{{ cookiecutter.namespace }}.namespace.yaml')
+# If the extensions has not been installed yet but we running directly from the git repo
+if not os.path.exists(ndx_icephys_meta_specpath):
+    {{ cookiecutter.py_pkg_name }}_specpath  = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                                            '../../../spec/',
+                                                                            '{{ cookiecutter.namespace }}.namespace.yaml'))
+# Load the namespace 
 load_namespaces({{ cookiecutter.py_pkg_name }}_specpath)
