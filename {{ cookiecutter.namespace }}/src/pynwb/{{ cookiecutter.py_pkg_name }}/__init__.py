4d38f9a66b87afea1dd7b101b73176134fb302fa
Ryan Ly <rly@lbl.gov>
2019-10-11 17:02:39 -0700

    Fix -- hyphens not allowed in var names

--- {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
+++ {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
@@ -1,6 +1,4 @@
 import os
 from pynwb import load_namespaces
-{{ cookiecutter.namespace }}_specpath = os.path.join(os.path.dirname(__file__),
-                                                     'spec',
-                                                     '{{ cookiecutter.namespace }}.namespace.yaml')
-load_namespaces({{ cookiecutter.namespace }}_specpath)
+{{ cookiecutter.py_pkg_name }}_specpath = os.path.join(os.path.dirname(__file__), 'spec', '{{ cookiecutter.namespace }}.namespace.yaml')
+load_namespaces({{ cookiecutter.py_pkg_name }}_specpath)
