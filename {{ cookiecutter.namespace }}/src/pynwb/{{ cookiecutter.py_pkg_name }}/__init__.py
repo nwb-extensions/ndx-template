a94b5343817b8498fba65ec4a3b1057d04989502
Ryan Ly <rly@lbl.gov>
2025-06-16 15:13:47 -0700

    Update __init__.py

--- {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
+++ {{ cookiecutter.namespace }}/src/pynwb/{{ cookiecutter.py_pkg_name }}/__init__.py
@@ -1,5 +1,4 @@
 from importlib.resources import files
-import os
 from pynwb import load_namespaces, get_class
 
 # Get path to the namespace.yaml file with the expected location when installed not in editable mode
@@ -7,7 +6,7 @@ __location_of_this_file = files(__name__)
 __spec_path = __location_of_this_file / "spec" / "{{ cookiecutter.namespace }}.namespace.yaml"
 
 # If that path does not exist, we are likely running in editable mode. Use the local path instead
-if not os.path.exists(__spec_path):
+if not __spec_path.exists():
     __spec_path = __location_of_this_file.parent.parent.parent / "spec" / "{{ cookiecutter.namespace }}.namespace.yaml"
 
 # Load the namespace
@@ -25,4 +24,4 @@ __all__ = [
 ]
 
 # Remove these functions/modules from the package
-del load_namespaces, get_class, files, os, __location_of_this_file, __spec_path
+del load_namespaces, get_class, files, __location_of_this_file, __spec_path
